deaa3d18b1171ddae242e9d8c952ef9673ca2037
riddhiman.adib <riddhiman.adib@harriken.com>
2017-05-26 22:48:35 +0600

    Added new activity

---
+++ app/src/main/java/me/riddhimanadib/readlater/ui/activity/AddLinkActivity.kt
@@ -0,0 +1,14 @@
+package me.riddhimanadib.readlater.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import me.riddhimanadib.readlater.R
+
+class AddLinkActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_link)
+    }
+}
