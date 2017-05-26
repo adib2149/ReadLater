beeccb72d2881aa77f5b2f4f0b79959b1c5d95fd
riddhiman.adib <riddhiman.adib@harriken.com>
2017-05-26 23:16:51 +0600

    Handle any link

--- app/src/main/java/me/riddhimanadib/readlater/ui/activity/AddLinkActivity.kt
+++ app/src/main/java/me/riddhimanadib/readlater/ui/activity/AddLinkActivity.kt
@@ -1,8 +1,7 @@
 package me.riddhimanadib.readlater.ui.activity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import android.support.v7.app.AppCompatActivity
 import me.riddhimanadib.readlater.R
 
 class AddLinkActivity : AppCompatActivity() {
@@ -10,5 +9,8 @@ class AddLinkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_link)
+
+        println(intent.data)
+
     }
 }
